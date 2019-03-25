318e8e4856a3256c8b09032370a5abc7e5da09f3
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-03-25 16:28:37 -0700

    Define create controller action

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,7 +1,22 @@
 class Api::V1::RecipesController < ApplicationController
+  before_action :find_recipe, only: [:update]
 
   def index
     @recipes = Recipe.all
     render json: @recipes
+  end
+
+  def create
+
+  end
+
+  private
+
+  def recipe_params
+    params.permit(:title, :ingredient_blob, :directions, :cooking_time, :image_url, :source_url)
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
