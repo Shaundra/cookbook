ce0775ca06a634600f9db693534161466b2c1f2d
Shaundra <9601515+Shaundra@users.noreply.github.com>
2019-03-26 13:24:55 -0700

    Define Tag class and its display functions, define createRecipe form

--- backend/app/models/tag_serializer.rb
+++ backend/app/models/tag_serializer.rb
@@ -2,5 +2,5 @@ class TagSerializer < ActiveModel::Serializer
   has_many :recipe_tags
   has_many :recipes, through: :recipe_tags
 
-  attributes :name, :recipes, :created_at, :updated_at
+  attributes :id, :name, :recipes, :created_at, :updated_at
 end
